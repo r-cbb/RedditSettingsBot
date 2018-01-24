95f35700e765c911cda1635d3d0a5fc9436f9512
cinciforthewin <tangemandavid@gmail.com>
2018-01-24 15:09:54 -0500

    Added Credentials_example.py

---
+++ credentials_example.py
@@ -0,0 +1,8 @@
+#Example Credentials file.  Script uses credentials.py
+
+app_ua = 'App User Agent Example'
+app_id = '1234567890ABCD'
+app_secret = '1234567890ABCDEFGHIJKLMNOPQ'
+app_refresh = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ123'
+
+# app_scopes = ['identity','history','modconfig','read','wikiedit','wikiread']
